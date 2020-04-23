35cb96b30ddab1a63fbb9d5900f6d6c1c1fe2c3f
nica18morena <jessica.frierson@live.com>
2020-04-22 20:59:18 -0700

    Transition current UI to new UI (work in progress)

---
+++ app/src/main/java/com/example/pictoevents/UI/CalendarFragment.kt
@@ -0,0 +1,110 @@
+package com.example.pictoevents.UI
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.Toast
+
+import com.example.pictoevents.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [CalendarFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [CalendarFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CalendarFragment : Fragment() {
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }*/
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val calendarView = view.findViewById<CalendarView>(R.id.calendarView)
+        val selectedDate = calendarView.date
+
+        /*calendarView.setOnDateChangeListener{view, year, month, dayOfMonth ->
+            val msg = "Date changed"
+            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+        }*/
+    }
+    // TODO: Rename method, update argument and hook method into UI event
+    /*fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }*/
+
+    /*override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }*/
+
+    /*override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }*/
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    /*interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }*/
+
+    /*companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment CalendarFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            CalendarFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }*/
+}
--- app/src/main/java/com/example/pictoevents/UI/CameraFragment.kt
+++ app/src/main/java/com/example/pictoevents/UI/CameraFragment.kt
@@ -1,106 +1,89 @@
-package com.example.pictoevents
+package com.example.pictoevents.UI
 
 import android.Manifest
-import android.content.pm.PackageManager
+import android.content.Context
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.util.Size
-import android.view.Surface
-import android.view.TextureView
-import android.view.ViewGroup
+import android.view.*
+import android.widget.ImageButton
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
+import androidx.fragment.app.Fragment
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
 import com.example.pictoevents.Calendar.CalendarObject
 import com.example.pictoevents.Calendar.CalendarObjectsGenerator
 import com.example.pictoevents.Calendar.PictoCalendar
 import com.example.pictoevents.OCREngine.IOCREngine
 import com.example.pictoevents.OCREngine.OCREngineFreeOCR
+import com.example.pictoevents.R
 import com.example.pictoevents.Util.FileManager
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import java.io.File
 import java.util.concurrent.Executors
 
-// Request permission
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+private val TAG: String? = CameraFragment::class.java.simpleName
 private const val REQUEST_CODE_PERMISSIONS = 10
-// Array of all permissions specified in the manifest file
-private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA, Manifest.permission.READ_CALENDAR,
+private val REQUIRED_PERMISSIONS = arrayOf(
+    Manifest.permission.CAMERA, Manifest.permission.READ_CALENDAR,
     Manifest.permission.WRITE_CALENDAR, Manifest.permission.INTERNET)
-//Class variables
-private val TAG: String? = MainActivity::class.java.simpleName
-//private val file = File(Environment.getExternalStorageDirectory().toString() + "/Pictoevents_${System.currentTimeMillis()}.jpg")
-//private val fileBase = File(Environment.getExternalStorageDirectory().toString() + "/Pictoevents/")
-//private val OCREngine: IOCREngine = OCREngineTesseract()
 private val OCREngine: IOCREngine = OCREngineFreeOCR()
-//private val fileManager = FileManager()
-//private const val storageBucket = "gs://pictoevents"
 private val cloudStorage = Firebase.storage
-//private lateinit var appBarConfig : AppBarConfiguration
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [CameraFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [CameraFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CameraFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    //private var param1: String? = null
+    //private var param2: String? = null
+    //private var listener: OnFragmentInteractionListener? = null
 
-class MainActivity : AppCompatActivity(), LifecycleOwner {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //Create NavHostFragment
-        val host : NavHostFragment = supportFragmentManager.findFragmentById(R.id.host_fragment)
-                as NavHostFragment? ?: return
-
-        val navController = host.navController
-        //appBarConfig = AppBarConfiguration(navController.graph)
-        setupBottomNavMenu(navController)
-
-        //Create actionbar
-        //var toolbar: ActionBar = ActionBar
-
-        //Create calendar
-        /*val calendarView = findViewById<CalendarView>(R.id.calendarView)
-        val selectedDate = calendarView.getDate()
-
-        calendarView.setOnDateChangeListener{view, year, month, dayOfMonth ->
-            val msg = "Date changed"
-            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
-        }*/
-
-
-        //Line 53 -69 is camera related. can comment out for now to get the UI going
-        /*viewFinder = findViewById(R.id.view_finder)
-
-        *///Request  permissions
-        if (!allPermissionsGranted()){
-            ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
         }
+    }*/
 
-        // Every time the provided texture view changes, recompute layout
-       /* viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
-            updateTransform()
-        }
-        Log.d(TAG,"Camera created")*/
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera, container, false)
     }
-    private fun setupBottomNavMenu(navController : NavController){
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
-        bottomNav?.setupWithNavController(navController)
-    }
-    // Add this after onCreate
+
     private val executor = Executors.newSingleThreadExecutor()
-    private lateinit var viewFinder: TextureView
 
-    private fun startCamera() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val camViewFinder = view.findViewById<TextureView>(R.id.view_finder)
+
+
+        camViewFinder.post { startCamera(camViewFinder, view) }
+
+        camViewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform(camViewFinder)
+        }
+    }
+    private fun startCamera(viewFinder: TextureView, view: View) {
         // Create configuration object for the viewfinder use case
         val previewConfig = PreviewConfig.Builder().apply {
             setTargetResolution(Size(640, 480))
@@ -119,7 +102,7 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
             parent.addView(viewFinder, 0)
 
             viewFinder.surfaceTexture = it.surfaceTexture
-            updateTransform()
+            updateTransform(viewFinder)
         }
         Log.d(TAG,"Viewfinder use case created")
         // Create configuration object for the image capture use case
@@ -132,12 +115,11 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
             }.build()
 
         // Build the image capture use case and attach button click listener
-        // This is commented out only to test updating the initial UI
-        /*val imageCapture = ImageCapture(imageCaptureConfig)
-        findViewById<ImageButton>(R.id.capture_button).setOnClickListener {
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        view.findViewById<ImageButton>(R.id.imageButton).setOnClickListener {
             /*val file = File(externalMediaDirs.first(),
                 "${System.currentTimeMillis()}.jpg")*/
-            val fileBase = File(externalMediaDirs.first(), "/")
+            val fileBase = File(this.requireContext().externalMediaDirs.first(), "/")
             FileManager.setFileBase(fileBase)
             FileManager.prepareDirectory(
                 FileManager.getFileBase())
@@ -153,18 +135,12 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
                         exc: Throwable?
                     ) {
                         val msg = "Photo capture failed: $message"
-                        Log.e("CameraXApp", msg, exc)
-                        viewFinder.post {
-                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                        }
+                        Log.e("TAG", msg, exc)
                     }
 
                     override fun onImageSaved(file: File) {
                         val msg = "Photo capture succeeded: ${file.absolutePath}"
-                        Log.d("CameraXApp", msg)
-                        viewFinder.post {
-                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                        }
+                        Log.d("TAG", msg)
                         // Once the image is saved, set the image location so it can be found by
                         // the OCR engine
                         OCREngine.setImageFileLocation(file)
@@ -179,10 +155,9 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         // If Android Studio complains about "this" being not a LifecycleOwner
         // try rebuilding the project or updating the appcompat dependency to
         // version 1.1.0 or higher.
-        CameraX.bindToLifecycle(this, preview, imageCapture)*/
+        CameraX.bindToLifecycle(this, preview, imageCapture)
     }
-
-    private fun updateTransform() {
+    private fun updateTransform(viewFinder : TextureView) {
         val matrix = Matrix()
 
         // Compute the center of the view finder
@@ -203,52 +178,6 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         viewFinder.setTransform(matrix)
     }
 
-    /**
-     * Process result from permission request dialog box, has the request
-     * been granted? If yes, start Camera. Otherwise display a toast
-     */
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                viewFinder.post { startCamera() }
-            } else {
-                Toast.makeText(this,
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
-    }
-
-    /**
-     * Check if all permission specified in the manifest have been granted
-     */
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    /** Create an instance of the OCR engine and process OCR*/
-    private fun processOCR() {
-        //Setup file directory and context for OCR
-        val dataPath = File(externalMediaDirs.first(), "/tessdata")
-        FileManager.setDataPath(dataPath.toString())
-        FileManager.prepareDirectory(
-            FileManager.getDataPath())
-        OCREngine.setContext(this)
-
-        //retrieve the image
-        //val bitmapUri = OCREngine.getCaptureImageOutputUri()
-        OCREngine.prepareOCREngine()
-
-        //process OCR- need bitmap...TODO: Bitmap is empty rn- need to properly create it
-        val bitmap = BitmapFactory.decodeFile(OCREngine.getImageFileLocation().toString())
-        //val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, bitmapUri)
-        val ocrText = OCREngine.extractText(bitmap)
-        Log.d(TAG, "OCR text is: $ocrText")
-    }
-
     private fun uploadFileToStorage(file: File)
     {
         val cloudStorageRef = cloudStorage.reference.child("images/${FileManager.getFileName()}")
@@ -267,21 +196,94 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         }
     }
 
+    private fun processOCR() {
+        //Setup file directory and context for OCR
+        val dataPath = File(this.requireContext().externalMediaDirs.first(), "/tessdata")
+        FileManager.setDataPath(dataPath.toString())
+        FileManager.prepareDirectory(
+            FileManager.getDataPath())
+        OCREngine.setContext(this.requireContext())
+
+        //retrieve the image
+        //val bitmapUri = OCREngine.getCaptureImageOutputUri()
+        OCREngine.prepareOCREngine()
+
+        //process OCR- need bitmap...TODO: Bitmap is empty rn- need to properly create it
+        val bitmap = BitmapFactory.decodeFile(OCREngine.getImageFileLocation().toString())
+        //val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, bitmapUri)
+        val ocrText = OCREngine.extractText(bitmap)
+        Log.d(TAG, "OCR text is: $ocrText")
+    }
+
     fun createCalEvent()
     {
         val text = OCREngine.getOCRResults() // Get OCR text
         val generateCalendarObjects = CalendarObjectsGenerator(text)
         generateCalendarObjects.identifyCalendarComponents() // identify from text all relevant components
 
-        val calendar = PictoCalendar(this)// Instance of PictoCalander to get the calendar ID
+        val calendar = PictoCalendar(this.requireContext())// Instance of PictoCalander to get the calendar ID
         calendar.checkCalendars()// This finds all calendars and assigned the calendarID to the Picto cal
 
         val formatter = generateCalendarObjects.getObjectFormatter() // Get the formatter to format all the data
         val calObject = CalendarObject(formatter.getFormattedHour(),formatter.getFormattedMin(), 0,
             formatter.getFormattedDay(), formatter.getFormattedMonth(), formatter.getFormattedYear(),
             formatter.getFormattedAMPM(), calendar.getCalId())
-        // TODO: Create the cal event
+
         calendar.setCalObj(calObject)
         calendar.buildCalEvent()
     }
+    // TODO: Rename method, update argument and hook method into UI event
+    /*fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }*/
+
+    /*override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }*/
+
+    /*override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }*/
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    /*interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }*/
+
+    /*companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment CameraFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            CameraFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }*/
 }
