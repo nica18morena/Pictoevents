c8834fb51557a419c20a12f1ea05cfd0cc44de53
nica18morena <jessica.frierson@live.com>
2020-10-01 00:26:36 -0700

    remove google key

--- app/src/main/java/com/example/pictoevents/NaturalLangProc/AnalyzeEntities.kt
+++ app/src/main/java/com/example/pictoevents/NaturalLangProc/AnalyzeEntities.kt
@@ -1,4 +1,12 @@
 package com.example.pictoevents.NaturalLangProc
 
 class AnalyzeEntities {
+
+    // Create instance of Language client
+
+    // Analyse
+
+    // parse response
+
+    // Organize to create Title
 }
