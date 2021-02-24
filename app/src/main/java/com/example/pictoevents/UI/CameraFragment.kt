625ea343f4059f5e049a4518df1ebe018189b457
nica18morena <jessica.frierson@live.com>
2021-02-23 23:25:33 -0800

    cleanup camera fragment

--- app/src/main/java/com/example/pictoevents/UI/CameraFragment.kt
+++ app/src/main/java/com/example/pictoevents/UI/CameraFragment.kt
@@ -100,20 +100,6 @@ package com.example.pictoevents.UI
         /** Blocking camera operations are performed using this executor */
         private lateinit var cameraExecutor: ExecutorService
 
-        /** Volume down button receiver used to trigger shutter */
-        /*private val volumeDownReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                when (intent.getIntExtra(KEY_EVENT_EXTRA, KeyEvent.KEYCODE_UNKNOWN)) {
-                    // When the volume down button is pressed, simulate a shutter button click
-                    KeyEvent.KEYCODE_VOLUME_DOWN -> {
-                        val shutter = container
-                            .findViewById<ImageButton>(R.id.camera_capture_button)
-                        shutter.simulateClick()
-                    }
-                }
-            }
-        }*/
-
         /**
          * We need a display listener for orientation changes that do not trigger a configuration
          * change, for example if we choose to override config change in manifest or for 180-degree
@@ -131,25 +117,13 @@ package com.example.pictoevents.UI
             } ?: Unit
         }
 
-        /*override fun onResume() {
-            super.onResume()
-            // Make sure that all permissions are still present, since the
-            // user could have removed them while the app was in paused state.
-            if (!MainActivity.hasPermissions(requireContext())) {
-                Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
-                    CameraFragmentDirections.actionCameraToPermissions()
-                )
-            }
-        }*/
-
         override fun onDestroyView() {
             super.onDestroyView()
 
             // Shut down our background executor
             cameraExecutor.shutdown()
 
-            // Unregister the broadcast receivers and listeners
-            //broadcastManager.unregisterReceiver(volumeDownReceiver)
+            // Unregister the and listeners
             displayManager.unregisterDisplayListener(displayListener)
         }
 
@@ -158,24 +132,6 @@ package com.example.pictoevents.UI
             container: ViewGroup?,
             savedInstanceState: Bundle?): View? =
             inflater.inflate(R.layout.fragment_camera, container, false)
-
-        /*private fun setGalleryThumbnail(uri: Uri) {
-            // Reference of the view that holds the gallery thumbnail
-            val thumbnail = container.findViewById<ImageButton>(R.id.photo_view_button)
-
-            // Run the operations in the view's thread
-            thumbnail.post {
-
-                // Remove thumbnail padding
-                thumbnail.setPadding(resources.getDimension(R.dimen.stroke_small).toInt())
-
-                // Load thumbnail into circular button using Glide
-                Glide.with(thumbnail)
-                    .load(uri)
-                    .apply(RequestOptions.circleCropTransform())
-                    .into(thumbnail)
-            }
-        }*/
 
         @SuppressLint("MissingPermission")
         override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -189,15 +145,8 @@ package com.example.pictoevents.UI
 
             broadcastManager = LocalBroadcastManager.getInstance(view.context)
 
-            // Set up the intent filter that will receive events from our main activity
-            //val filter = IntentFilter().apply { addAction(KEY_EVENT_ACTION) }
-            //broadcastManager.registerReceiver(volumeDownReceiver, filter)
-
             // Every time the orientation of device changes, update rotation for use cases
             displayManager.registerDisplayListener(displayListener, null)
-
-            // Determine the output directory
-            //outputDirectory = MainActivity.getOutputDirectory(requireContext())
 
             // Wait for the views to be properly laid out
             viewFinder.post {
@@ -265,24 +214,6 @@ package com.example.pictoevents.UI
                     .setTargetRotation(rotation)
                     .build()
 
-                // ImageAnalysis
-                /*imageAnalyzer = ImageAnalysis.Builder()
-                    // We request aspect ratio but no resolution
-                    .setTargetAspectRatio(screenAspectRatio)
-                    // Set initial target rotation, we will have to call this again if rotation changes
-                    // during the lifecycle of this use case
-                    .setTargetRotation(rotation)
-                    .build()
-                    // The analyzer can then be assigned to the instance
-                    .also {
-                        it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
-                            // Values returned from our analyzer are passed to the attached listener
-                            // We log image analysis results here - you should do something useful
-                            // instead!
-                            Log.d(TAG, "Average luminosity: $luma")
-                        })
-                    }*/
-
                 // Must unbind the use-cases before rebinding them
                 cameraProvider.unbindAll()
 
@@ -328,26 +259,12 @@ package com.example.pictoevents.UI
                 container.removeView(it)
             }*/
 
-            // Inflate a new view containing all UI for controlling the camera
-            //val controls = View.inflate(requireContext(), R.layout.camera_ui_container, container)
-
-            // In the background, load latest photo taken (if any) for gallery thumbnail
-            /*lifecycleScope.launch(Dispatchers.IO) {
-                outputDirectory.listFiles { file ->
-                    EXTENSION_WHITELIST.contains(file.extension.toUpperCase(Locale.ROOT))
-                }?.max()?.let {
-                    setGalleryThumbnail(Uri.fromFile(it))
-                }
-            }*/
-
             // Listener for button used to capture photo
             //controls.findViewById<ImageButton>(R.id.camera_capture_button).setOnClickListener {
                 captureButton.setOnClickListener{
                 // Get a stable reference of the modifiable image capture use case
                 imageCapture?.let { imageCapture ->
 
-                    /*val file = File(externalMediaDirs.first(),
-                "${System.currentTimeMillis()}.jpg")*/
                     val fileBase = File(this.requireContext().externalMediaDirs.first(), "/")
                     FileManager.setFileBase(fileBase)
                     FileManager.prepareDirectory(
@@ -355,9 +272,6 @@ package com.example.pictoevents.UI
                     val imageName = "${System.currentTimeMillis()}.png"
                     FileManager.setFileName(imageName)
                     val photoFile = File(FileManager.getFileBase(), "/$imageName")
-
-                    // Create output file to hold the image
-                    //val photoFile = createFile(outputDirectory, FILENAME, PHOTO_EXTENSION)
 
                     // Setup image capture metadata
                     val metadata = Metadata().apply {
@@ -382,20 +296,6 @@ package com.example.pictoevents.UI
                                 val savedUri = output.savedUri ?: Uri.fromFile(photoFile)
                                 Log.d(TAG, "Photo capture succeeded: $savedUri")
 
-                                // We can only change the foreground Drawable using API level 23+ API
-                                /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                                    // Update the gallery thumbnail with latest picture taken
-                                    setGalleryThumbnail(savedUri)
-                                }*/
-
-                                // Implicit broadcasts will be ignored for devices running API level >= 24
-                                // so if you only target API level 24+ you can remove this statement
-                                /*if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-                                    requireActivity().sendBroadcast(
-                                        Intent(android.hardware.Camera.ACTION_NEW_PICTURE, savedUri)
-                                    )
-                                }*/
-
                                 // If the folder selected is an external media directory, this is
                                 // unnecessary but otherwise other apps will not be able to access our
                                 // images unless we scan them using [MediaScannerConnection]
@@ -417,39 +317,8 @@ package com.example.pictoevents.UI
                             }
                         })
 
-                    // We can only change the foreground Drawable using API level 23+ API
-                    /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-                        // Display flash animation to indicate that photo was captured
-                        container.postDelayed({
-                            container.foreground = ColorDrawable(Color.WHITE)
-                            container.postDelayed(
-                                { container.foreground = null }, ANIMATION_FAST_MILLIS)
-                        }, ANIMATION_SLOW_MILLIS)
-                    }*/
                 }
             }
-            // Listener for button used to switch cameras
-            /*controls.findViewById<ImageButton>(R.id.camera_switch_button).setOnClickListener {
-                lensFacing = if (CameraSelector.LENS_FACING_FRONT == lensFacing) {
-                    CameraSelector.LENS_FACING_BACK
-                } else {
-                    CameraSelector.LENS_FACING_FRONT
-                }
-                // Re-bind use cases to update selected camera
-                bindCameraUseCases()
-            }*/
-
-            // Listener for button used to view the most recent photo
-            /*controls.findViewById<ImageButton>(R.id.photo_view_button).setOnClickListener {
-                // Only navigate when the gallery has photos
-                if (true == outputDirectory.listFiles()?.isNotEmpty()) {
-                    Navigation.findNavController(
-                        requireActivity(), R.id.fragment_container
-                    ).navigate(CameraFragmentDirections
-                        .actionCameraToGallery(outputDirectory.absolutePath))
-                }
-            }*/
         }
 
         private fun processOCR() {
@@ -513,92 +382,6 @@ package com.example.pictoevents.UI
             //Create txt file
             FileManager.createOCRTextFile(text)
         }
-        /**
-         * Our custom image analysis class.
-         *
-         * <p>All we need to do is override the function `analyze` with our desired operations. Here,
-         * we compute the average luminosity of the image by looking at the Y plane of the YUV frame.
-         */
-        /*private class LuminosityAnalyzer(listener: LumaListener? = null) : ImageAnalysis.Analyzer {
-            private val frameRateWindow = 8
-            private val frameTimestamps = ArrayDeque<Long>(5)
-            private val listeners = ArrayList<LumaListener>().apply { listener?.let { add(it) } }
-            private var lastAnalyzedTimestamp = 0L
-            var framesPerSecond: Double = -1.0
-                private set
-
-            /**
-             * Used to add listeners that will be called with each luma computed
-             */
-            fun onFrameAnalyzed(listener: LumaListener) = listeners.add(listener)
-
-            /**
-             * Helper extension function used to extract a byte array from an image plane buffer
-             */
-            private fun ByteBuffer.toByteArray(): ByteArray {
-                rewind()    // Rewind the buffer to zero
-                val data = ByteArray(remaining())
-                get(data)   // Copy the buffer into a byte array
-                return data // Return the byte array
-            }
-
-            /**
-             * Analyzes an image to produce a result.
-             *
-             * <p>The caller is responsible for ensuring this analysis method can be executed quickly
-             * enough to prevent stalls in the image acquisition pipeline. Otherwise, newly available
-             * images will not be acquired and analyzed.
-             *
-             * <p>The image passed to this method becomes invalid after this method returns. The caller
-             * should not store external references to this image, as these references will become
-             * invalid.
-             *
-             * @param image image being analyzed VERY IMPORTANT: Analyzer method implementation must
-             * call image.close() on received images when finished using them. Otherwise, new images
-             * may not be received or the camera may stall, depending on back pressure setting.
-             *
-             */
-            override fun analyze(image: ImageProxy) {
-                // If there are no listeners attached, we don't need to perform analysis
-                if (listeners.isEmpty()) {
-                    image.close()
-                    return
-                }
-
-                // Keep track of frames analyzed
-                val currentTime = System.currentTimeMillis()
-                frameTimestamps.push(currentTime)
-
-                // Compute the FPS using a moving average
-                while (frameTimestamps.size >= frameRateWindow) frameTimestamps.removeLast()
-                val timestampFirst = frameTimestamps.peekFirst() ?: currentTime
-                val timestampLast = frameTimestamps.peekLast() ?: currentTime
-                framesPerSecond = 1.0 / ((timestampFirst - timestampLast) /
-                        frameTimestamps.size.coerceAtLeast(1).toDouble()) * 1000.0
-
-                // Analysis could take an arbitrarily long amount of time
-                // Since we are running in a different thread, it won't stall other use cases
-
-                lastAnalyzedTimestamp = frameTimestamps.first
-
-                // Since format in ImageAnalysis is YUV, image.planes[0] contains the luminance plane
-                val buffer = image.planes[0].buffer
-
-                // Extract image data from callback object
-                val data = buffer.toByteArray()
-
-                // Convert the data into an array of pixel values ranging 0-255
-                val pixels = data.map { it.toInt() and 0xFF }
-
-                // Compute average luminance for the image
-                val luma = pixels.average()
-
-                // Call all listeners with new value
-                listeners.forEach { it(luma) }
-
-                image.close()
-            }
-        }*/
 
         companion object {
 
